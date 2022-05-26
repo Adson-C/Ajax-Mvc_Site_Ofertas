28f7398b126cd75281e77889e7ed46c73105143e
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-25 23:56:39 -0300

    projeto iniciado

---
+++ src/main/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.ads.adsajaxspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdsAjaxSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdsAjaxSpringBootApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.adsajaxspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsAjaxSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
