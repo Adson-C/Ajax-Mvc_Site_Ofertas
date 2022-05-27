76e60ff06f6041c1e53d4dc7f997d4137880715b
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-26 23:25:52 -0300

    Criando BD ,Templates layout, css , images

---
+++ src/main/java/com/ads/adsajaxspringboot/web/controller/HomerController.java
@@ -0,0 +1,14 @@
+package com.ads.adsajaxspringboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomerController {
+	
+	@GetMapping("/")
+	public String init() {
+		return "promo-add"; // a class raiz da aplicação
+	}
+
+}
