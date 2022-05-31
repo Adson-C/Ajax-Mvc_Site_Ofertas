a2911b3104bf08c183dfc3d5d87a644d617d1eb9
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-05-30 23:23:07 -0300

    Class Controller para Metas Tag

--- src/main/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplication.java
+++ src/main/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplication.java
@@ -1,32 +1,15 @@
 package com.ads.adsajaxspringboot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ads.adsajaxspringboot.domain.SocialMetaTag;
-import com.ads.adsajaxspringboot.service.SocialMetaTagService;
-
 @SpringBootApplication
-public class AdsAjaxSpringBootApplication  implements CommandLineRunner {
+public class AdsAjaxSpringBootApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdsAjaxSpringBootApplication.class, args);
 		
 	}
 	
-	@Autowired
-	SocialMetaTagService service;
-
-	@Override
-	public void run(String... args) throws Exception {
-		
-		SocialMetaTag tag = service.getSocialMetaTagByUrl("https://www.boticario.com.br/");
-		System.out.println(tag.toString());
-
-
-		
-	}
 
 }
