c52127bda904b3eb3e7f0f61978d54ecd09aaee3
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-06-10 00:10:47 -0300

    ordenando lista de promoçoes por pagina

--- src/main/java/com/ads/adsajaxspringboot/web/controller/PromocaoController.java
+++ src/main/java/com/ads/adsajaxspringboot/web/controller/PromocaoController.java
@@ -7,6 +7,7 @@ import com.ads.adsajaxspringboot.repository.PromocaoRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -39,10 +40,10 @@ public class PromocaoController {
     @GetMapping("/list")
     public String listarOferta(ModelMap model) {
         Sort sort = Sort.by(Sort.Direction.DESC, "dtCadastro");
-        model.addAttribute("promocoes", promocaoRepository.findAll(sort));
+        PageRequest pageRequest = PageRequest.of(0, 8, sort);
+        model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
         return "promo-list";
     }
-
     // ===================================ADD OFERTAS ==========================================
 
     @PostMapping("/save")
