9f7eacd13311bcb023640eebb2988cc02bd6de79
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-06-11 00:28:21 -0300

    Infinite Scroll em ação

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,10 +1,16 @@
 var pageNumber = 0;
+
+$(document).ready(function() {
+    $("#loader-img").hide();
+    $("#fim-btn").hide();
+});
+
 // efeito infinit de scroll
 $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
     var conteudo = $(document).height() - $(window).height();
 
-    console.log('scrollTop', scrollTop, ' | ', 'conteudo', conteudo);
+    // console.log('scrollTop', scrollTop, ' | ', 'conteudo', conteudo);
 
     if(scrollTop >= conteudo) {
         pageNumber++ ;
@@ -22,10 +28,27 @@ function loadByScrollBar(pageNumber) {
         data: {
             page: pageNumber
         },
+        beforeSend: function() {
+            $("#loader-img").show();
+        },
+        success: function(response) {
+           //  console.log("resposta >", respopnse);
 
-        success: function(respopnse) {
-            console.log("resposta >", respopnse);
+           if(response.length > 150) {
+
+                $(".row").fadeIn(250, function() {
+                        $(this).append(response);
+                 });
+           }else {
+                $("#fim-btn").show();
+                $("#loader-img").removeClass("loader");
+           }
+        },
+        error: function(xhr) {
+            alert("Ops, Ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+        },
+        complete: function() {
+           $("#loader-img").hide();
         }
-
     });
 }
