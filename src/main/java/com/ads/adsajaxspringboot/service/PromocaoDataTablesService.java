392a3ef6d1bc5d95c39715b469d10df4396274ae
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-06-24 23:06:06 -0300

    Fitrando por preço

--- src/main/java/com/ads/adsajaxspringboot/service/PromocaoDataTablesService.java
+++ src/main/java/com/ads/adsajaxspringboot/service/PromocaoDataTablesService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Sort;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.math.BigDecimal;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -49,6 +50,13 @@ public class PromocaoDataTablesService {
 
             return repository.findAll(pageable);
         }
+
+        if (search.matches("^[0-9]+([.,][0-9]{2})?$")) {
+            search  = search.replace(",",".");
+            return repository.findByPreco(new BigDecimal(search), pageable);
+
+        }
+        
         return repository.findByTituloOrSiteOrCategoria(search, pageable);
     }
     private String searchBy(HttpServletRequest request) {
