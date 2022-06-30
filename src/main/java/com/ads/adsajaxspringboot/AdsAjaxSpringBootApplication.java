264cfed651d0c09a89d97fe2456b75410590755f
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-06-29 23:38:51 -0300

    Integrando a DWR ao Spring

--- src/main/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplication.java
+++ src/main/java/com/ads/adsajaxspringboot/AdsAjaxSpringBootApplication.java
@@ -1,8 +1,13 @@
 package com.ads.adsajaxspringboot;
 
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
+@ImportResource(locations = "classpath:dwr-spring.xml")
 @SpringBootApplication
 public class AdsAjaxSpringBootApplication  {
 
@@ -10,6 +15,17 @@ public class AdsAjaxSpringBootApplication  {
 		SpringApplication.run(AdsAjaxSpringBootApplication.class, args);
 		
 	}
-	
+
+	@Bean
+	public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet() {
+		DwrSpringServlet dwrServlet = new DwrSpringServlet();
+
+		ServletRegistrationBean<DwrSpringServlet> registrationBean =
+				new ServletRegistrationBean<>(dwrServlet, "/dwr/*");
+
+		registrationBean.addInitParameter("debug", "true");
+		registrationBean.addInitParameter("activeReverseAjaxEnabled", "true");
+		return registrationBean;
+	}
 
 }
